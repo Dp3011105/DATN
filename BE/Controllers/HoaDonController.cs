d6568847db3c905537bb7e6e9b08787c7174799d
Tanaka <tnk3atan@gmail.com>
2025-08-06 09:10:23 +0700

    ThongKe

--- BE/Controllers/HoaDonController.cs
+++ BE/Controllers/HoaDonController.cs
@@ -1,53 +1,62 @@
 using BE.models;
 using Microsoft.AspNetCore.Mvc;
-using Repository.IRepository;
 
-namespace BE.Controllers
+[Route("api/[controller]")]
+[ApiController]
+public class HoaDonController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class HoaDonController : ControllerBase
+    private readonly IHoaDonRepository _repository;
+
+    public HoaDonController(IHoaDonRepository repository) => _repository = repository;
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+        => Ok(await _repository.GetAllAsync());
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(int id)
     {
-        private readonly IHoaDonRepository _repository;
+        var result = await _repository.GetByIdAsync(id);
+        return result == null ? NotFound() : Ok(result);
+    }
 
-        public HoaDonController(IHoaDonRepository repository)
+    [HttpPost]
+    public async Task<IActionResult> Create(HoaDonCreateDTO dto)
+    {
+        var model = new HoaDon
         {
-            _repository = repository;
-        }
+            ID_Khach_Hang = dto.ID_Khach_Hang,
+            ID_Nhan_Vien = dto.ID_Nhan_Vien,
+            ID_Hinh_Thuc_Thanh_Toan = dto.ID_Hinh_Thuc_Thanh_Toan,
+            ID_Dia_Chi = dto.ID_Dia_Chi,
+            ID_Phi_Ship = dto.ID_Phi_Ship,
+            Dia_Chi_Tu_Nhap = dto.Dia_Chi_Tu_Nhap,
+            Ngay_Tao = dto.Ngay_Tao,
+            Tong_Tien = dto.Tong_Tien,
+            Phi_Ship = dto.Phi_Ship,
+            Trang_Thai = dto.Trang_Thai,
+            Ghi_Chu = dto.Ghi_Chu,
+            Ma_Hoa_Don = dto.Ma_Hoa_Don,
+            Loai_Hoa_Don = dto.Loai_Hoa_Don,
+            LyDoHuyDon = dto.LyDoHuyDon,
+            LyDoDonHangCoVanDe = dto.LyDoDonHangCoVanDe
+        };
 
-        [HttpGet]
-        public async Task<IActionResult> GetAll()
-        {
-            var result = await _repository.GetAllAsync();
-            return Ok(result);
-        }
+        await _repository.AddAsync(model);
+        return Ok();
+    }
 
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id)
-        {
-            var result = await _repository.GetByIdAsync(id);
-            return result == null ? NotFound() : Ok(result);
-        }
+    [HttpPut]
+    public async Task<IActionResult> Update(HoaDon model)
+    {
+        await _repository.UpdateAsync(model);
+        return Ok();
+    }
 
-        [HttpPost]
-        public async Task<IActionResult> Create(HoaDon model)
-        {
-            await _repository.AddAsync(model);
-            return Ok();
-        }
-
-        [HttpPut]
-        public async Task<IActionResult> Update(HoaDon model)
-        {
-            await _repository.UpdateAsync(model);
-            return Ok();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            await _repository.DeleteAsync(id);
-            return Ok();
-        }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(int id)
+    {
+        await _repository.DeleteAsync(id);
+        return Ok();
     }
 }
--- BE/Repository/IRepository/IHoaDonRepository.cs
+++ BE/Repository/IRepository/IHoaDonRepository.cs
@@ -1,13 +1,10 @@
 using BE.models;
 
-namespace Repository.IRepository
+public interface IHoaDonRepository
 {
-    public interface IHoaDonRepository
-    {
-        Task<IEnumerable<HoaDon>> GetAllAsync();
-        Task<HoaDon?> GetByIdAsync(int id);
-        Task AddAsync(HoaDon entity);
-        Task UpdateAsync(HoaDon entity);
-        Task DeleteAsync(int id);
-    }
+    Task<IEnumerable<HoaDonDTO>> GetAllAsync();
+    Task<HoaDon?> GetByIdAsync(int id);
+    Task AddAsync(HoaDon entity);
+    Task UpdateAsync(HoaDon entity);
+    Task DeleteAsync(int id);
 }
